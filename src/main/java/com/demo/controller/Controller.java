a8242472a7282fb8a3c9719d7d485f4e9d790a3f
Alexpandiyan <alexpandiyan.chokkan@mindzen.co.in>
2018-02-04 17:44:27 +0530

    src added

--- src/main/java/com/demo/controller/Controller.java
+++ src/main/java/com/demo/controller/Controller.java
@@ -1,16 +1,28 @@
 package com.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.model.Customer;
+import com.demo.service.CustomerService;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
 
+	@Autowired
+	private CustomerService customerService;
+	
 	@GetMapping("/hello")
 	public String hello(){
 		return "Hello Murali";
+	}
+	
+	@GetMapping("/get-customer")
+	public Customer getCustomer(){
+		return customerService.findCustomer();	
 	}
 
 }
--- src/main/java/com/demo/service/CustomerServiceImpl.java
+++ src/main/java/com/demo/service/CustomerServiceImpl.java
@@ -1,16 +1,23 @@
 package com.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.demo.model.Customer;
+import com.demo.repository.CustomerRepository;
 
 @Service
 public class CustomerServiceImpl implements CustomerService{
 
+	@Autowired
+	private CustomerRepository customerRepository;
+
 	@Override
 	public Customer findCustomer() {
-		// TODO Auto-generated method stub
-		return null;
+		Customer customer = new Customer();
+		customer.setName("Murli");
+		customerRepository.save(customer);
+		return customerRepository.findOne(1L);
 	}
 
 }
